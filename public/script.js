9027f531683a2f7ee001a00157e5c7882f09da31
krishnaa <krishnaa2902@gmail.com>
2023-12-23 12:47:52 +0530

    first commit

--- public/script.js
+++ public/script.js
@@ -1,3 +1,8 @@
+window.addEventListener("beforeunload", function(e){
+  // Do something
+  e.preventDefault();
+}, false);
+
 const socket = io('http://localhost:3000')
 
 const msgerForm = get(".msger-inputarea");
@@ -9,20 +14,108 @@ const roomContainer = document.getElementById('room-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const left_IMG = "https://www.linkpicture.com/q/profile-icon-2.png";
-const right_IMG = "https://www.linkpicture.com/q/profile-icon-1.png";
+const left_IMG = "img/profile-pic.jpg";
+const right_IMG = "img/profile-pic.jpg";
+
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+function gcd(a, b) {
+  if (b == 0) {
+      return a;
+  }
+  else if (b == 1) {
+      return 1;
+  }
+  return gcd(b, a % b);
+}
+
+function modInverse(a, n) {
+  for (let i = 1; i < n; i++) {
+      if ((a * i) % n == 1) {
+          return i;
+      }
+  }
+  return -1;
+}
+
+function isPrime(n) {
+  if(n==2) {
+    return true;
+  }
+  else if (n%2==0 || n<2) {
+    return false;
+  }
+  else {
+    for (let i = 3; i <= Math.sqrt(n); i+=2) {
+      if(n%i==0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+function generateKeys() {
+  let p = 101;
+  let q = 31;
+  while(1) {
+    p = Number(prompt("Enter a prime number : "));
+    if(isPrime(p)) {
+      break;
+    }
+  }
+  while(1) {
+    q = Number(prompt("Enter another prime number : "));
+    if(isPrime(q) && p!=q && ((p*q) > 255)) {
+      break;
+    }
+  }
+  const n = p * q;
+  const phi = (p - 1) * (q - 1);
+  let e = Math.floor(Math.random() * (500 - 10 + 1)) + 10;
+  while (1) {
+    if (gcd(e, phi) == 1) {
+      break;
+    }
+    e = Math.floor(Math.random() * (200 - 10 + 1)) + 10;
+  }
+  console.log("e = " + e);
+  const d = modInverse(e, phi);
+  const publicKey = { e, n };
+  const privateKey = { d, n };
+  return [publicKey, privateKey];
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+let ourPrivateKey = [0,0];
+let ourPublicKey = [0,0];
 
 if (messageForm != null) {
   const name = prompt('What is your name?')
-  appendMessage("You",right_IMG,"right","You joined")
-  socket.emit('new-user', roomName, name)
+  const [publicKey, privateKey] = generateKeys();
+  ourPrivateKey = privateKey;
+
+  // ourPublicKey = publicKey;
+  // console.log(publicKey);
+  // console.log(ourPublicKey+"ourPublicKey");
+  const {e,n} = publicKey;
+  ourPublicKey[0] = e
+  ourPublicKey[1] = n
+  appendMessage(`You | (${e},${n}) `,null,right_IMG,"right","You joined")
+  socket.emit('new-user', roomName, name, publicKey, privateKey)
 
 msgerForm.addEventListener('submit', e => {
   e.preventDefault()
-  const message = msgerInput.value
+  const message = msgerInput.value;
   
-  appendMessage("You",right_IMG,"right",message)
-  socket.emit('send-chat-message', roomName, message)
+  appendMessage("You",publicKey,right_IMG,"right",message)
+  socket.emit('send-chat-message', publicKey,roomName, message)
   msgerInput.value = ''
 })
 }
@@ -37,16 +130,34 @@ socket.on('room-created', room => {
   roomContainer.append(roomLink)
 })
 
-socket.on('chat-message', data => {
-  appendMessage(data.name,left_IMG,"left",data.message)
+socket.on('chat-message', (publicKey,data) => {
+  const ct = data.message.split(',');
+  console.log(ct)
+  let plaintext = "";
+  for (let i = 0; i < ct.length; i++) {
+    plaintext += String.fromCharCode(decrypt(ct[i], ourPrivateKey) % 255);
+  }
+  console.log(plaintext);
+  // appendMessage(data.name,publicKey, left_IMG,"left",data.message)
+  const pK = "("+data.e+","+data.n+")";
+  const opK = "("+ourPublicKey[0]+","+ourPublicKey[1]+")";
+  if(pK == opK) {
+    appendMessage(data.name,publicKey, left_IMG,"left",plaintext);
+  }
+  
 })
 
-socket.on('user-connected', name => {
-  appendMessage("NOTE!!",left_IMG,"left",`${name} has entered the chat :)`)
+socket.on('chat-message-all', (publicKey,data) => {
+  appendMessage(data.name,publicKey, left_IMG,"left",data.message);
+})
+
+socket.on('user-connected', (name,publicKey) => {
+  const {e,n} = publicKey;
+  appendMessage("NOTE!!",null,left_IMG,"left",`${name} has entered the chat with public key (${e},${n}) :)`)
 })
 
 socket.on('user-disconnected', name => {
-  appendMessage("NOTE!!",left_IMG,"left",`${name} has left the chat :(`)
+  appendMessage("NOTE!!",null,left_IMG,"left",`${name} has left the chat :(`)
 })
 
 function get(selector, root = document) {
@@ -60,7 +171,30 @@ function formatDate(date) {
   return `${h.slice(-2)}:${m.slice(-2)}`;
 }
 
-function appendMessage(name, img, side, text) {
+////////////////////////////////////////////////////////////////////////
+function encrypt(m, publicKey) {
+  const { e, n } = publicKey;
+  return modExp(m, e, n);
+}
+
+function decrypt(c, privateKey) {
+  const { d, n } = privateKey;
+  return modExp(c, d, n);
+}
+
+function modExp(base, exponent, modulus) {
+  var result = 1;
+  for (var i = 0; i < exponent; i++) {
+      result = (result * base) % modulus;
+  }
+  return result;
+}
+////////////////////////////////////////////////////////////////////////
+
+
+function appendMessage(name, publicKey, img, side, text) {
+
+  if(publicKey == null) {
   const msgHTML = `
     <div class="msg ${side}-msg">
       <div class="msg-img" style="background-image: url(${img})"></div>
@@ -76,4 +210,90 @@ function appendMessage(name, img, side, text) {
   `;
   msgerChat.insertAdjacentHTML("beforeend", msgHTML);
   msgerChat.scrollTop += 500;
+  }
+  else {
+    const {e,n} = publicKey;
+    const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name} | (${e},${n})</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500; 
+  }
+
 }
+
+
+//////////////
+
+
+// function modExp(base, exponent, modulus) {
+//   var result = 1;
+//   for (var i = 0; i < exponent; i++) {
+//       result = (result * base) % modulus;
+//   }
+//   return result;
+// }
+// function gcd(a, b) {
+//   if (b == 0) {
+//       return a;
+//   }
+//   else if (b == 1) {
+//       return 1;
+//   }
+//   return gcd(b, a % b);
+// }
+
+// function modInverse(a, n) {
+//   for (let i = 1; i < n; i++) {
+//       if ((a * i) % n == 1) {
+//           return i;
+//       }
+//   }
+//   return -1;
+// }
+
+// function encrypt(m, publicKey) {
+//   const { e, n } = publicKey;
+//   return modExp(m, e, n);
+// }
+
+// function decrypt(c, privateKey) {
+//   const { d, n } = privateKey;
+//   return modExp(c, d, n);
+// }
+
+// function generateKeys() {
+//   const p = Number(prompt("Enter a prime number : "));
+//   const q = Number(prompt("Enter another prime number : "));
+//   const n = p * q;
+//   const phi = (p - 1) * (q - 1);
+//   let e = 19;
+//   const d = modInverse(e, phi);
+//   const publicKey = { e, n };
+//   const privateKey = { d, n };
+//   return [publicKey, privateKey];
+// }
+
+
+// const [publicKey, privateKey] = generateKeys();
+// let message = "krishnaa";
+// let c_arr = [];
+// for (let i = 0; i < message.length; i++) {
+//   c_arr.push(encrypt(message.charCodeAt(i), publicKey));
+// }
+// console.log(c_arr)
+// let plaintext = "";
+// for (let i = 0; i < c_arr.length; i++) {
+//   plaintext += String.fromCharCode(decrypt(c_arr[i], privateKey));
+// }
+// console.log(plaintext);
--- server.js
+++ server.js
@@ -32,14 +32,86 @@ app.get('/:room', (req, res) => {
 
 server.listen(3000)
 
+///
+function extractMessage(message) {
+
+
+  // Define a regular expression to match the pattern (number, number): message
+  const regex = /\((\d+),(\d+)\): (.+)/;
+  
+  // Use the regular expression to extract the values and the message
+  const match = message.match(regex);
+  let e = 0;
+  let n = 0;
+  let messageAfter = "";
+  if (match) {
+    e = match[1];  // Extract the first number, 19
+    n = match[2];  // Extract the second number, 881
+    messageAfter = match[3];  // Extract the message, "what is going on"
+  
+    console.log(`group1 = ${e}`);
+    console.log(`group2 = ${n}`);
+    console.log(`messageAfter = ${messageAfter}`);
+    const publicKey = {e,n};
+    return [publicKey, messageAfter];
+  } 
+  else {
+    console.log("Pattern not found in the message.");
+    const defaultPublicKey = [0,0];
+    return [defaultPublicKey,message];
+  }
+
+}
+
+
+
+function encrypt(m, publicKey) {
+  const { e, n } = publicKey;
+  return modExp(m, e, n);
+}
+
+function decrypt(c, privateKey) {
+  const { d, n } = privateKey;
+  return modExp(c, d, n);
+}
+
+function modExp(base, exponent, modulus) {
+  var result = 1;
+  for (var i = 0; i < exponent; i++) {
+      result = (result * base) % modulus;
+  }
+  return result;
+}
+
+///
+
+
 io.on('connection', socket => {
-  socket.on('new-user', (room, name) => {
+  socket.on('new-user', (room, name, publicKey, privateKey) => {
     socket.join(room)
     rooms[room].users[socket.id] = name
-    socket.to(room).broadcast.emit('user-connected', name)
+    socket.to(room).broadcast.emit('user-connected', name, publicKey)
   })
-  socket.on('send-chat-message', (room, message) => {
-    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+  socket.on('send-chat-message', (publicKey, room, message) => {
+    const [publicKeySender,msg] = extractMessage(message);
+    const mesg = msg;
+    const {e,n} = publicKeySender;
+    console.log("e : " + e + ", n : " + n);
+    console.log("mesg : " + msg);
+    let c_arr = [];
+    let c_string = ""
+    let len = mesg.length;
+    for (let i = 0; i < mesg.length; i++) {
+      c_arr.push(encrypt(mesg.charCodeAt(i), publicKeySender));
+      c_string += encrypt(mesg.charCodeAt(i), publicKeySender);
+    }
+    if(!e && !n) {
+      socket.to(room).broadcast.emit('chat-message-all', publicKey, { message: message, name: rooms[room].users[socket.id] })
+    }
+    else {
+      socket.to(room).broadcast.emit('chat-message', publicKey, { message: c_arr.toString(), name: rooms[room].users[socket.id], e: e, n: n })
+    }
+    
   })
   socket.on('disconnect', () => {
     getUserRooms(socket).forEach(room => {
